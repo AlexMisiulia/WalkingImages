77749380b13b2ac4855ff40e5d6732b28b349ae8
Alexey Misiulia <alexeymisiulia@gmail.com>
2019-08-16 21:01:10 +0300

    [ADD] Glide placeholders

--- app/src/main/java/com/simplyfire/komoottesttask/feature/photolist/PhotoAdapter.kt
+++ app/src/main/java/com/simplyfire/komoottesttask/feature/photolist/PhotoAdapter.kt
@@ -7,8 +7,10 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.simplyfire.komoottesttask.R
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.simplyfire.komoottesttask.core.utils.inflateAdapterView
+
 
 private val diffUtilCallback =  object: DiffUtil.ItemCallback<DisplayablePhoto>() {
     override fun areItemsTheSame(oldItem: DisplayablePhoto, newItem: DisplayablePhoto): Boolean {
@@ -22,19 +24,27 @@ private val diffUtilCallback =  object: DiffUtil.ItemCallback<DisplayablePhoto>(
 }
 
 class PhotoAdapter : ListAdapter<DisplayablePhoto, PhotoAdapter.PhotoViewHolder>(diffUtilCallback) {
+    private val glideOptions = RequestOptions()
+        .fitCenter()
+        .placeholder(com.simplyfire.komoottesttask.R.drawable.loading)
+        .error(com.simplyfire.komoottesttask.R.drawable.img_error)
+        .diskCacheStrategy(DiskCacheStrategy.ALL)
+        .dontAnimate()
+        .dontTransform()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
-        return PhotoViewHolder(parent.inflateAdapterView(R.layout.photo_item_view))
+        return PhotoViewHolder(parent.inflateAdapterView(com.simplyfire.komoottesttask.R.layout.photo_item_view))
     }
 
     override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 
-    class PhotoViewHolder(private val v: View) : RecyclerView.ViewHolder(v) {
-        private val imageView = v.findViewById<ImageView>(R.id.imageView)
+    inner class PhotoViewHolder(private val v: View) : RecyclerView.ViewHolder(v) {
+        private val imageView = v.findViewById<ImageView>(com.simplyfire.komoottesttask.R.id.imageView)
 
         fun bind(photo: DisplayablePhoto) {
-            Glide.with(v).load(photo.url).into(imageView)
+            Glide.with(v).load(photo.url).apply(glideOptions).into(imageView)
         }
     }
 }

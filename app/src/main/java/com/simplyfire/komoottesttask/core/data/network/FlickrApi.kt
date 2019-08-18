55654190bcf36d69b090e1826fb9b10aa1cd1664
Alexey Misiulia <alexeymisiulia@gmail.com>
2019-08-18 14:12:47 +0300

    [ADD] radius param 1 km to search photos api request

--- app/src/main/java/com/simplyfire/komoottesttask/core/data/network/FlickrApi.kt
+++ app/src/main/java/com/simplyfire/komoottesttask/core/data/network/FlickrApi.kt
@@ -7,7 +7,6 @@ import retrofit2.http.Query
 
 
 private const val API_KEY = "1dd80d460f02b16a80eaa28eecee5e7c"
-private const val SECRET_KEY = "08530f4c468e2d97"
 
 const val API_BASE_URL = "https://www.flickr.com/services/rest/"
 private const val JSON_FORMAT_QUERY = "format=json&nojsoncallback=1"
@@ -15,7 +14,7 @@ private const val GET_PHOTO_METHOD_QUERY = "method=flickr.photos.search"
 
 interface FlickrApi {
 
-    @GET("?$GET_PHOTO_METHOD_QUERY&api_key=$API_KEY&$JSON_FORMAT_QUERY&extras=url_c")
+    @GET("?$GET_PHOTO_METHOD_QUERY&api_key=$API_KEY&$JSON_FORMAT_QUERY&extras=url_c&radius=1")
     fun searchPhotos(@Query("lat")latitude: String, @Query("lon")longitude: String)
     : Observable<SearchPhotosResponse>
 }
